5a93e0e0566bc7532083cd4c6b4a118e15aa23e3
OleksandrKokriashkin <kokriashkin.aleksandr@gmail.com>
2019-03-04 20:37:53 +0200

    fix sh

--- step-definitions/actions.js
+++ step-definitions/actions.js
@@ -1,9 +1,9 @@
 const {Given, When} = require('cucumber');
 
-Given('User prints comment {string}', (comment) => {
+Given(' User navigates to the {string} page with url {string}', (comment) => {
     return console.log(comment);
 });
 
-When('User navigates to the {string} page with url {string}', (_, url) => {
+When('User click "" {string}', (_, url) => {
     return browser.get(url);
 });
